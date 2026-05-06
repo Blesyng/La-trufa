f2c5fb7c7b7d489a0cdff885de1133455077ac7d
Anderson Santos <Andersonfs19979@gmail.com>
2026-05-06 12:17:53 -0300

    feat: add la-doces v1.1 in subdirectory

---
+++ v1.1/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'la-doces-v1.1-cache-v2';
+const urlsToCache = [
+  '/',
+  '/la-doces-app.html',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
